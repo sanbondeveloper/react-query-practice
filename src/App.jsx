863cd19f1443e5ee621e3a8a071e51c2b7909136
sanbondeveloper <sanbondeveloper@gmail.com>
2023-12-17 19:14:43 +0900

    💄 UI 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useTodosQuery } from './hooks/useTodos';
 
 const App = () => {
   const { data: todos } = useTodosQuery();
 
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
+    <>
+      <Title>TODO</Title>
+      <TodoList>
+        {todos.map(({ id, title, completed }) => (
+          <TodoItem key={id}>
+            <input type="checkbox" checked={completed} />
+            <div className="title">{title}</div>
+          </TodoItem>
+        ))}
+      </TodoList>
+    </>
   );
 };
 
 export default App;
+
+const Title = styled.h1``;
+
+const TodoList = styled.ul`
+  list-style: none;
+  padding-left: 0px;
+  height: 500px;
+  overflow-y: scroll;
+`;
+
+const TodoItem = styled.li`
+  margin: 0px 5px;
+  padding: 20px 0px;
+  display: flex;
+  align-items: center;
+
+  .title {
+    margin-left: 10px;
+  }
+
+  & + li {
+    border-top: 1px solid #ccc;
+  }
+`;
